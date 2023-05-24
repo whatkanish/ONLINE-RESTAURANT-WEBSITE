238d7f39585c5e76ba51969577e9ffc0000dc0ce
KANISHTHIKA SINGH <flaviasingh1234@gmail.com>
2023-05-25 05:06:09 +0530

    first  commit '#' will be ignored, and an empty message aborts the commit.

---
+++ script.js
@@ -0,0 +1,9 @@
+let menu = document.querySelector('#menu-bar');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () =>{
+
+  menu.classList.toggle('fa-times');
+  navbar.classList.toggle('active');
+
+}
